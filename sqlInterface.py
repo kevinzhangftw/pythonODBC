f53a96fa74a37997f42fc8fb99677e924b39531d
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2018-03-30 15:04:45 -0700

    sql interface can call db and save to variables

--- sqlInterface.py
+++ sqlInterface.py
@@ -1,6 +1,15 @@
+import pymssql
+
+def getMaxPassengerID():
+    mycursor = conn.cursor()
+    mycursor.execute('select max(passenger_id) from Passenger')
+    row = mycursor.fetchone()
+    mycursor.close()
+    # row is tuple like this (99537,), so select only first element
+    return row[0]
+
 def createProfile(firstname, lastname):
-    largestPassenger_id = 1234 #SELECT MAX(passenger_id) FROM Passenger
-    passenger_id = largestPassenger_id + 1
+    passenger_id = getMaxPassengerID() + 1
     miles = 0
     # INSERT ON Passenger first_name last_name passenger_id miles
     print("The profile for passenger {} {} {} was created.".format(passenger_id, firstname, lastname))
@@ -37,7 +46,16 @@ def addBooking(passenger_id):
 # Tests
 # createProfile("kai", "z")
 # findby(1,2)
-addBooking(1234)
+# addBooking(1234)
 
 # UI
+def main():
+    global conn
+    conn = pymssql.connect(host='cypress.csil.sfu.ca', user='s_kwz', password='4YTdnH4gEGqnYJ2M', database='kwz354')
+    createProfile("kai", "z")
+
+
+
+if __name__ == "__main__":
+    main()
 
--- sqlconn.py
+++ sqlconn.py
@@ -4,11 +4,11 @@ import pymssql
 conn = pymssql.connect(host='cypress.csil.sfu.ca', user='s_kwz', password='4YTdnH4gEGqnYJ2M', database='kwz354')
 
 mycursor = conn.cursor()
-mycursor.execute('SELECT * from Passenger')
+mycursor.execute('select max(passenger_id) from Passenger')
 row = mycursor.fetchone()
-while row:
-    print(row)
-    row = mycursor.fetchone()
-
+# while row:
+#     print(row)
+#     row = mycursor.fetchone()
+print(row)
 mycursor.close()
 
