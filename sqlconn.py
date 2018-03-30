ba2ce2a0c4d06ca3a13393c1255ae91ac8ba6444
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2018-03-30 13:49:28 -0700

    updated sqlconn to calldb and retrieve passengers

--- sqlconn.py
+++ sqlconn.py
@@ -3,15 +3,12 @@ import pymssql
 
 conn = pymssql.connect(host='cypress.csil.sfu.ca', user='s_kwz', password='4YTdnH4gEGqnYJ2M', database='kwz354')
 
-cur = conn.cursor()
-
-# to validate the connection, there is no need to change the following line
-cur.execute('SELECT username from dbo.helpdesk')
-row = cur.fetchone()
+mycursor = conn.cursor()
+mycursor.execute('SELECT * from Passenger')
+row = mycursor.fetchone()
 while row:
+    print(row)
+    row = mycursor.fetchone()
 
-    print (("SQL Server standard login name= %s") %  (row[0]))
-    # from Python version 3: print is a function, not a statement.
-    row = cur.fetchone()
+mycursor.close()
 
-conn.close()
