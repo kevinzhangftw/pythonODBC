1b6bac3d8bcdb770eeffd6fe0fd1542b88d3d680
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2018-04-05 13:37:25 -0700

    refactored addbooking() added error catching for insertbooking to db

--- sqlInterface.py
+++ sqlInterface.py
@@ -19,9 +19,14 @@ def insertPassengerToDB(passengerTuple):
 
 def insertBooking(passenger_id, flight_code, depart_date):
     mycursor = conn.cursor()
-    mycursor.execute('insert into Booking values (%s, %s, %d)', (flight_code,depart_date,passenger_id))
-    conn.commit()
-    print('Booking has been successfully inserted into database')
+    try:
+        mycursor.execute('insert into Booking values (%s, %s, %d)', (flight_code,depart_date,passenger_id))
+        conn.commit()
+        print('Booking has been successfully inserted into database')
+        return True
+    except:
+        print('Insertion Failed: Possible Duplicate Insertion')
+        return False
 
 def createProfile(firstname, lastname):
     passenger_id = getMaxPassengerID() + 1
@@ -69,41 +74,51 @@ def verifyPassengerid(passenger_id):
         mycursor  = conn.cursor()
         mycursor.execute("SELECT first_name, last_name FROM Passenger WHERE passenger_id = %d", passenger_id)
         row = mycursor.fetchone()
-        print (row[0], row[1])
+        print ('Adding Booking for :', row[0], row[1])
         mycursor.close()
         return True
     except:
         print('no such instance')
-        return False    
+        return False  
+
+def processSingleTrip(passenger_id):
+    print("your chose single trip")
+    inputSpec = input("specify the flight_code and depart_date. For example: JA100 2016-11-28 >> ").split()
+    flight_code = inputSpec[0]
+    depart_date = inputSpec[1]
+    if verifyFlightInstance(flight_code, depart_date):
+        if insertBooking(passenger_id, flight_code, depart_date):
+            print('Booking has been successfully added to database')
+        else:
+            processSingleTrip(passenger_id)
+    else:
+        print ("Flight Instance Not found. Please try again")
+        addBooking() 
+
+def processMultiTrip(passenger_id):
+    print("your chose multi trip")
+    inputSpec1 = input("specify your first flight_code and depart_date. For example: JA260 2016-11-29 >> ").split()
+    flight_code1 = inputSpec1[0]
+    depart_date1 = inputSpec1[1]
+    print("fligt_code1:", flight_code1)
+    print("depart_date1:", depart_date1)
+
+    inputSpec2 = input("specify your second flight_code and depart_date.  the “depart_date” of the second leg has to be no earlier than the “depart_date” of the first leg. For example: 12345 1990/11/03 ").split()
+    flight_code2 = inputSpec2[0]
+    depart_date2 = inputSpec2[1]
+    print("fligt_code2:", flight_code2)
+    print("depart_date2:", depart_date2)
+
 
 def addBooking():
-    passenger_id = input('Please enter passenger id For example: 22050 >>')
+    passenger_id = input('Please enter passenger id For example: 22050 >> ')
     if verifyPassengerid(passenger_id):
-        tripInput=input("is this a single trip or multi-city trip? enter “single” or “multi” >>")
+        
+        tripInput=input("is this a single trip or multi-city trip? enter “single” or “multi” >> ")
         if tripInput == 'single':
-            print("your chose single trip")
-            inputSpec = input("specify the flight_code and depart_date. For example: JA100 2016-11-28 ").split()
-            flight_code = inputSpec[0]
-            depart_date = inputSpec[1]
-            if verifyFlightInstance(flight_code, depart_date):
-                insertBooking(passenger_id, flight_code, depart_date)
-            else:
-                print ("Flight Instance Not found. Please try again")
-                addBooking(passenger_id)
-
+            processSingleTrip(passenger_id)
         elif tripInput == 'multi':
-            print("your chose multi trip")
-            inputSpec1 = input("specify your first flight_code and depart_date. For example: JA260 2016-11-29 ").split()
-            flight_code1 = inputSpec1[0]
-            depart_date1 = inputSpec1[1]
-            print("fligt_code1:", flight_code1)
-            print("depart_date1:", depart_date1)
-
-            inputSpec2 = input("specify your second flight_code and depart_date.  the “depart_date” of the second leg has to be no earlier than the “depart_date” of the first leg. For example: 12345 1990/11/03 ").split()
-            flight_code2 = inputSpec2[0]
-            depart_date2 = inputSpec2[1]
-            print("fligt_code2:", flight_code2)
-            print("depart_date2:", depart_date2)
+            processMultiTrip(passenger_id)
         else:
             print("wrong input!")
     else:
@@ -128,9 +143,9 @@ def main():
     # createProfile("june", "kim")
     # viewPassengers()
     # findby('JA300','2016/11/28')
-    # addBooking()
-    verifyPassengerid(22050)
-    # verifyFlightInstance('JA100','2017-11-28')
+    addBooking()
+    # verifyPassengerid(22050)
+    # verifyFlightInstance('JA100','2016-11-28')
     
 
 if __name__ == "__main__":
