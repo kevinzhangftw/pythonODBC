c26c16c12f5250f081f4de78238c62b817887ac0
Kevin Zhang <kevin.c.t.zhang@gmail.com>
2018-04-09 12:06:02 -0700

    testing py2 print0

--- demopy2.py
+++ demopy2.py
@@ -2,11 +2,24 @@
 import pymssql
 from datetime import datetime
 
+# UI
+def appEntry():
+    userInput = input('please select the following options: create profile, view passengers, or add booking >> ')
+    if userInput == 'create profile':
+        print('createProfile()')
+    elif userInput == 'view passengers':
+        print('viewPassengers()')
+    elif userInput == 'add booking':
+        print('addBooking()')
+    else:
+        print('umm..we didnt find any matched options. you can say: create profile OR view passengers OR add booking')
+        appEntry()
+
 def main():
     global conn
     conn = pymssql.connect(host='cypress.csil.sfu.ca', user='s_kwz', password='4YTdnH4gEGqnYJ2M', database='kwz354')
     print('Welcome to manage your flight app!')
-    # appEntry()
+    appEntry()
 
 if __name__ == "__main__":
     main()
